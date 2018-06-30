2af6d693d83f9ebeede98b48df965fbfc250b79d
OstapHetman <prickly4528@gmail.com>
2018-06-30 11:22:27 +0300

    Understanding fetch method

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,9 +4,10 @@ let deferredPrompt;
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/service-worker.js')
-        .then( 
-            () => {
+        .then( () => {
             console.log('Service worker registered');    
+        }).catch( (err) => {
+            console.log(err)
         });
 }
 
@@ -17,3 +18,40 @@ window.addEventListener('beforeinstallprompt', (event) => {
     deferredPrompt = event;
     return false;
 });
+
+// Understanding fetch method (GET request)
+// fetch('http://httpbin.org/ip')
+fetch('http://httpbin.org/ips') // Wrong URL
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+// Understanding fetch method (POST request)
+fetch('http://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors',
+    body: JSON.stringify({
+       message: 'Does this work?'
+    })
+}) // POST URL
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
